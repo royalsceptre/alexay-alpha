562db3a10ba897234a6d5f83f9ec567ebbff8ae9
narakarax <liliyaogerenko@gmail.com>
2022-07-29 10:57:20 -0700

    commit

--- src/components/worksmanagement.tsx
+++ src/components/worksmanagement.tsx
@@ -6,7 +6,7 @@ const HowItWorks = () => {
                     <h2 className="text-[#333] text-[44px] font-extrabold m-[0 0 78px]">How it works</h2>
                 </div>
                 <ul className="pl-[0] m-0 l-[-11px]">
-                    <li className="bg-[url('./assets/img/General.jpg')] block mb-[61px] pl-[98px] bg-no-repeat"></li>
+                    <li className="bg-[url('./assets/img/General.jpg')] block mb-[61px] pl-[98px] bg-no-repeat "></li>
                     <li></li>
                     <li></li>
                 </ul>
