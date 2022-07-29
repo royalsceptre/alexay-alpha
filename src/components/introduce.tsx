b22f36d7a6f04c0bf124fad0c9f6bd20f9da6410
narakarax <liliyaogerenko@gmail.com>
2022-07-29 04:29:49 -0700

    APP-[729429]

--- src/components/introduce.tsx
+++ src/components/introduce.tsx
@@ -8,10 +8,10 @@ const Introduce = () => {
                         <picture>
                             <img src="https://ww2-secure.justanswer.com/static/sip/btc/membership-benefits/general_tnt_1_v1.jpg" className="rounded-full border-[10px] h-auto float-left w-[223px] block" alt="Chat with an Expert" />
                         </picture>
-                        <div className="float-left w-[calc(100%-264px)] text-[32px]">
+                        <div className="ml-[41px] float-left w-[calc(100%-264px)] text-[32px]">
                             Chat with an Expert 1:1
                         </div>
-                        <div className="text-[16px] float-left leading-[28px] mt-[20px] block text-[#000] ml-[43px]">
+                        <div className="text-[16px] float-left leading-[28px] block text-[#000] ml-[43px]">
                         Connect with verified Experts in minutes, 24/7. You can save hundreds every time you use it.
                         </div>
                     </div>
@@ -19,10 +19,10 @@ const Introduce = () => {
                         <picture>
                             <img src="https://ww2-secure.justanswer.com/static/sip/btc/membership-benefits/general_tnt_2_v1.jpg" className="rounded-full border-[10px] h-auto float-left w-[223px] block" alt="Chat with an Expert" />
                         </picture>
-                        <div className="float-left w-[calc(100%-264px)] text-[32px]">
+                        <div className="ml-[41px] float-left w-[calc(100%-264px)] text-[32px]">
                             Instant deals
                         </div>
-                        <div className="text-[16px] float-left leading-[28px] mt-[20px] block text-[#000] ml-[43px]">
+                        <div className="text-[16px] float-left leading-[28px] block text-[#000] ml-[43px]">
                         Enjoy savings at merchants nationwide, from restaurants and shopping to entertainment and travel, including Sam's Club, Office Depot, Jiffy Lube, Walt Disney World Resort, Best Western, and more.
                         </div>
                     </div>
@@ -30,10 +30,10 @@ const Introduce = () => {
                         <picture>
                             <img src="https://ww2-secure.justanswer.com/static/sip/btc/membership-benefits/general_tnt_3_v1.jpg" className="rounded-full border-[10px] h-auto float-left w-[223px] block" alt="Chat with an Expert" />
                         </picture>
-                        <div className="float-left w-[calc(100%-264px)] text-[32px]">
+                        <div className="ml-[41px] float-left w-[calc(100%-264px)] text-[32px]">
                             Global travel assistance
                         </div>
-                        <div className="text-[16px] float-left leading-[28px] mt-[20px] block text-[#000] ml-[43px]">
+                        <div className="text-[16px] float-left leading-[28px] block text-[#000] ml-[43px]">
                         Protect yourself with emergency benefits like medical evacuation, lost or stolen travel documents assistance, transportation to join a family member in the hospital, dental and pharmacy referrals, legal consultation, and more.
                         </div>
                     </div>
@@ -41,10 +41,10 @@ const Introduce = () => {
                         <picture>
                             <img src="https://ww2-secure.justanswer.com/static/sip/btc/membership-benefits/general_tnt_4_v1.jpg" className="rounded-full border-[10px] h-auto float-left w-[223px] block" alt="Chat with an Expert" />
                         </picture>
-                        <div className="float-left w-[calc(100%-264px)] text-[32px]">
+                        <div className="ml-[41px] float-left w-[calc(100%-264px)] text-[32px]">
                             Financial wellness
                         </div>
-                        <div className="text-[16px] float-left leading-[28px] mt-[20px] block text-[#000] ml-[43px]">
+                        <div className="text-[16px] float-left leading-[28px] block text-[#000] ml-[43px]">
                         Gain a helping hand as you navigate milestones like buying a house or paying for college. Speak with a financial counselor for personalized guidance on your budget, savings, and financial goals.
                         </div>
                     </div>
@@ -52,10 +52,10 @@ const Introduce = () => {
                         <picture>
                             <img src="https://ww2-secure.justanswer.com/static/JA54186/membership-savings.jpg" className="rounded-full border-[10px] h-auto float-left w-[223px] block" alt="Chat with an Expert" />
                         </picture>
-                        <div className="float-left w-[calc(100%-264px)] text-[32px]">
+                        <div className="ml-[41px] float-left w-[calc(100%-264px)] text-[32px]">
                         Only 49 €/month. Cancel anytime.
                         </div>
-                        <div className="text-[16px] float-left leading-[28px] mt-[20px] block text-[#000] ml-[43px]">
+                        <div className="text-[16px] float-left leading-[28px] block text-[#000] ml-[43px]">
                         Professional fees can add up, especially when you’re charged by the hour. Every answer you get on JustAnswer saves you an average of 140 €.
                         </div>
                     </div>
