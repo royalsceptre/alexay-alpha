f87549d6547a67580a63eb7b8c37cd4b218fc1a7
narakarax <liliyaogerenko@gmail.com>
2022-07-29 04:10:10 -0700

    APP-[729410]

---
+++ src/components/introduce.tsx
@@ -0,0 +1,24 @@
+const Introduce = () => {
+    return (
+        <>
+            <div className="2xl:w-[calc(100%-811px)] pl-[125px] pt-[80px]">
+                <div className="text-[44px] text-[#333] font-semibold leading-[59px] h-59px">What's included:</div>
+                <div className="mt-[44px] relative left-[-11px]">
+                    <div className="inline-block mb-[55px]">
+                        <picture>
+                            <img src="https://ww2-secure.justanswer.com/static/sip/btc/membership-benefits/general_tnt_1_v1.jpg" className="rounded-full border-[10px] h-auto float-left w-[223px] block" alt="Chat with an Expert" />
+                        </picture>
+                        <div className="ml-[41px] float-left w-[calc(100%-264px)] text-[32px]">
+                            Chat with an Expert 1:1
+                        </div>
+                        <div className="text-[20px] leading-[28px] mt-[20px] block text-[#000] font-semibold">
+                        Connect with verified Experts in minutes, 24/7. You can save hundreds every time you use it.
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </>
+    )
+}
+
+export default Introduce;
