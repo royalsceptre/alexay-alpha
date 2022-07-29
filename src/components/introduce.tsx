16cf1ae3e2a186c3221b3cf37c1f4afcccde862e
narakarax <liliyaogerenko@gmail.com>
2022-07-29 04:41:40 -0700

    complete

--- src/components/introduce.tsx
+++ src/components/introduce.tsx
@@ -1,68 +1,112 @@
 const Introduce = () => {
-    return (
-        <>
-            <div className="2xl:w-[calc(100%-811px)] pl-[125px] pt-[80px]">
-                <div className="text-[44px] text-[#333] font-semibold leading-[59px] h-59px">What's included:</div>
-                <div className="mt-[44px] relative left-[-11px]">
-                    <div className="inline-block mb-[55px]">
-                        <picture>
-                            <img src="https://ww2-secure.justanswer.com/static/sip/btc/membership-benefits/general_tnt_1_v1.jpg" className="rounded-full border-[10px] h-auto float-left w-[223px] block" alt="Chat with an Expert" />
-                        </picture>
-                        <div className="ml-[41px] float-left w-[calc(100%-264px)] text-[32px]">
-                            Chat with an Expert 1:1
-                        </div>
-                        <div className="text-[16px] float-left leading-[28px] block text-[#000] ml-[43px]">
-                        Connect with verified Experts in minutes, 24/7. You can save hundreds every time you use it.
-                        </div>
-                    </div>
-                    <div className="inline-block mb-[55px]">
-                        <picture>
-                            <img src="https://ww2-secure.justanswer.com/static/sip/btc/membership-benefits/general_tnt_2_v1.jpg" className="rounded-full border-[10px] h-auto float-left w-[223px] block" alt="Chat with an Expert" />
-                        </picture>
-                        <div className="ml-[41px] float-left w-[calc(100%-264px)] text-[32px]">
-                            Instant deals
-                        </div>
-                        <div className="text-[16px] float-left leading-[28px] block text-[#000] ml-[43px]">
-                        Enjoy savings at merchants nationwide, from restaurants and shopping to entertainment and travel, including Sam's Club, Office Depot, Jiffy Lube, Walt Disney World Resort, Best Western, and more.
-                        </div>
-                    </div>
-                    <div className="inline-block mb-[55px]">
-                        <picture>
-                            <img src="https://ww2-secure.justanswer.com/static/sip/btc/membership-benefits/general_tnt_3_v1.jpg" className="rounded-full border-[10px] h-auto float-left w-[223px] block" alt="Chat with an Expert" />
-                        </picture>
-                        <div className="ml-[41px] float-left w-[calc(100%-264px)] text-[32px]">
-                            Global travel assistance
-                        </div>
-                        <div className="text-[16px] float-left leading-[28px] block text-[#000] ml-[43px]">
-                        Protect yourself with emergency benefits like medical evacuation, lost or stolen travel documents assistance, transportation to join a family member in the hospital, dental and pharmacy referrals, legal consultation, and more.
-                        </div>
-                    </div>
-                    <div className="inline-block mb-[55px]">
-                        <picture>
-                            <img src="https://ww2-secure.justanswer.com/static/sip/btc/membership-benefits/general_tnt_4_v1.jpg" className="rounded-full border-[10px] h-auto float-left w-[223px] block" alt="Chat with an Expert" />
-                        </picture>
-                        <div className="ml-[41px] float-left w-[calc(100%-264px)] text-[32px]">
-                            Financial wellness
-                        </div>
-                        <div className="text-[16px] float-left leading-[28px] block text-[#000] ml-[43px]">
-                        Gain a helping hand as you navigate milestones like buying a house or paying for college. Speak with a financial counselor for personalized guidance on your budget, savings, and financial goals.
-                        </div>
-                    </div>
-                    <div className="inline-block mb-[55px]">
-                        <picture>
-                            <img src="https://ww2-secure.justanswer.com/static/JA54186/membership-savings.jpg" className="rounded-full border-[10px] h-auto float-left w-[223px] block" alt="Chat with an Expert" />
-                        </picture>
-                        <div className="ml-[41px] float-left w-[calc(100%-264px)] text-[32px]">
-                        Only 49 €/month. Cancel anytime.
-                        </div>
-                        <div className="text-[16px] float-left leading-[28px] block text-[#000] ml-[43px]">
-                        Professional fees can add up, especially when you’re charged by the hour. Every answer you get on JustAnswer saves you an average of 140 €.
-                        </div>
-                    </div>
-                </div>
+  return (
+    <>
+      <div className="2xl:w-[calc(100%-811px)] pl-[125px] pt-[80px]">
+        <div className="text-[44px] text-[#333] font-semibold leading-[59px] h-59px">
+          What's included:
+        </div>
+        <div className="mt-[44px] relative left-[-11px]">
+          <div className="inline-block mb-[55px]">
+            <picture>
+              <img
+                src="https://ww2-secure.justanswer.com/static/sip/btc/membership-benefits/general_tnt_1_v1.jpg"
+                className="rounded-full border-[10px] h-auto float-left w-[223px] block"
+                alt="Chat with an Expert"
+              />
+            </picture>
+            <div className="ml-[41px] float-left w-[calc(100%-264px)]">
+              <h3 className="text-[30px] leading-[44px] text-[#000] mt-[17px]">
+                Chat with an Expert 1:1
+              </h3>
+              <p className="text-[20px] float-left leading-[28px] block text-[#000]">
+                Connect with verified Experts in minutes, 24/7. You can save
+                hundreds every time you use it.
+              </p>
             </div>
-        </>
-    )
-}
+          </div>
+          <div className="inline-block mb-[55px]">
+            <picture>
+              <img
+                src="https://ww2-secure.justanswer.com/static/sip/btc/membership-benefits/general_tnt_2_v1.jpg"
+                className="rounded-full border-[10px] h-auto float-left w-[223px] block"
+                alt="Chat with an Expert"
+              />
+            </picture>
+            <div className="ml-[41px] float-left w-[calc(100%-264px)]">
+              <h3 className="text-[30px] leading-[44px] text-[#000] mt-[17px]">
+                Instant deals
+              </h3>
+              <p className="text-[20px] float-left leading-[28px] block text-[#000]">
+                Enjoy savings at merchants nationwide, from restaurants and
+                shopping to entertainment and travel, including Sam's Club,
+                Office Depot, Jiffy Lube, Walt Disney World Resort, Best
+                Western, and more.
+              </p>
+            </div>
+          </div>
+          <div className="inline-block mb-[55px]">
+            <picture>
+              <img
+                src="https://ww2-secure.justanswer.com/static/sip/btc/membership-benefits/general_tnt_3_v1.jpg"
+                className="rounded-full border-[10px] h-auto float-left w-[223px] block"
+                alt="Chat with an Expert"
+              />
+            </picture>
+            <div className="ml-[41px] float-left w-[calc(100%-264px)]">
+              <h3 className="text-[30px] leading-[44px] text-[#000] mt-[17px]">
+                Global travel assistance
+              </h3>
+              <p className="text-[20px] float-left leading-[28px] block text-[#000]">
+                Protect yourself with emergency benefits like medical
+                evacuation, lost or stolen travel documents assistance,
+                transportation to join a family member in the hospital, dental
+                and pharmacy referrals, legal consultation, and more.
+              </p>
+            </div>
+          </div>
+          <div className="inline-block mb-[55px]">
+            <picture>
+              <img
+                src="https://ww2-secure.justanswer.com/static/sip/btc/membership-benefits/general_tnt_4_v1.jpg"
+                className="rounded-full border-[10px] h-auto float-left w-[223px] block"
+                alt="Chat with an Expert"
+              />
+            </picture>
+            <div className="ml-[41px] float-left w-[calc(100%-264px)]">
+              <h3 className="text-[30px] leading-[44px] text-[#000] mt-[17px]">
+                Financial wellness
+              </h3>
+              <p className="text-[20px] float-left leading-[28px] block text-[#000]">
+                Gain a helping hand as you navigate milestones like buying a
+                house or paying for college. Speak with a financial counselor
+                for personalized guidance on your budget, savings, and financial
+                goals.
+              </p>
+            </div>
+          </div>
+          <div className="inline-block mb-[55px]">
+            <picture>
+              <img
+                src="https://ww2-secure.justanswer.com/static/JA54186/membership-savings.jpg"
+                className="rounded-full border-[10px] h-auto float-left w-[223px] block"
+                alt="Chat with an Expert"
+              />
+            </picture>
+            <div className="ml-[41px] float-left w-[calc(100%-264px)]">
+              <h3 className="text-[30px] leading-[44px] text-[#000] mt-[17px]">
+                Only 49 €/month. Cancel anytime.
+              </h3>
+              <p className="text-[20px] float-left leading-[28px] block text-[#000]">
+                Professional fees can add up, especially when you’re charged by
+                the hour. Every answer you get on JustAnswer saves you an
+                average of 140 €.
+              </p>
+            </div>
+          </div>
+        </div>
+      </div>
+    </>
+  );
+};
 
 export default Introduce;
