769d5be2df3ec1d53769f696e3380984ec191a05
narakarax <liliyaogerenko@gmail.com>
2022-07-29 13:20:29 -0700

    APP-[729120]

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,9 @@ import ImageSlider from "./components/imgslider";
 import Introduce from "./components/introduce";
 import Carousallayout from "./components/carouselbar";
 import HowItWorks from "./components/worksmanagement";
+import Review from "./components/reviewcarousel";
+import SecurityBage from "./components/securitybage";
+import CompanyMeeting from "./components/meeting";
 function App() {
   return (
     <div>
@@ -10,6 +13,9 @@ function App() {
         <Introduce />
         <Carousallayout />
         <HowItWorks />
+        <Review />
+        <SecurityBage />
+        <CompanyMeeting />
     </div>
   );
 }
---
+++ src/components/meeting.tsx
@@ -0,0 +1,12 @@
+const CompanyMeeting = () => {
+    return (
+        <>
+        <div className="bg-[url('https://ww2.justanswer.com/static/ja36640/btc_general_image_for_CTA_section.jpg.jpg')] bg-center bg-no-repeat h-[475px] flex flex-col">
+            <div className="2xl:w-[calc(100%-811px)] xl:w-[calc(100%-700px)] xl:pr-[48px] 2xl:pl-[125px]">
+            <h2 className="max-w-[550px] h-[103px] text-[#000] text-[32px] leading-[44px] text-center my-0 mx-auto">Join and get it all. Cancel anytime. No commitments.</h2>
+            </div>
+        </div>
+        </>
+    )
+}
+export default CompanyMeeting;
