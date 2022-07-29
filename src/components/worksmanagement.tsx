f21be8d9ac1e28a0818c434c913c49d4a76839d9
narakarax <liliyaogerenko@gmail.com>
2022-07-29 11:11:24 -0700

    APP-[7291111]

--- src/components/worksmanagement.tsx
+++ src/components/worksmanagement.tsx
@@ -2,14 +2,16 @@ const HowItWorks = () => {
     return (
         <>
             <div className="mt-[87px] mb-[93px]">
-                <div className="2xl:w-[calc(100% - 811px)] xl:w-[calc(100% - 700px)] pl-[125px] xl:pr-[48px]">
+                <div className="2xl:w-[calc(100%-811px)] xl:w-[calc(100%-700px)] pl-[125px] xl:pr-[48px]">
                     <h2 className="text-[#333] text-[44px] font-extrabold m-[0 0 78px]">How it works</h2>
-                </div>
                 <ul className="pl-[0] m-0 l-[-11px]">
-                    <li className="bg-[url('./assets/img/General.jpg')] block mb-[61px] pl-[98px] bg-no-repeat "></li>
+                    <li className="bg-[url('https://ww2-secure.justanswer.com/static/sip/btc/how-it-works/connect.svg')] block mb-[61px] pl-[98px] bg-no-repeat">
+                        <h3 className="text-[#000] text-[32px] leading-44px m-0">Connect</h3>
+                    </li>
                     <li></li>
                     <li></li>
                 </ul>
+                </div>
             </div>
         </>
     )
