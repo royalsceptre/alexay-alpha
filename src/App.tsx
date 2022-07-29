a079fe2d931a5d8905ed2690ba34ff51580879bd
narakarax <liliyaogerenko@gmail.com>
2022-07-29 10:53:08 -0700

    complete

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,15 @@
 import React from 'react';
 import ImageSlider from "./components/imgslider";
 import Introduce from "./components/introduce";
+import Carousallayout from "./components/carouselbar";
+import HowItWorks from "./components/worksmanagement";
 function App() {
   return (
     <div>
-      
         <ImageSlider />
         <Introduce />
+        <Carousallayout />
+        <HowItWorks />
     </div>
   );
 }
---
+++ src/components/worksmanagement.tsx
@@ -0,0 +1,18 @@
+const HowItWorks = () => {
+    return (
+        <>
+            <div className="mt-[87px] mb-[93px]">
+                <div className="2xl:w-[calc(100% - 811px)] xl:w-[calc(100% - 700px)] pl-[125px] xl:pr-[48px]">
+                    <h2 className="text-[#333] text-[44px] font-extrabold m-[0 0 78px]">How it works</h2>
+                </div>
+                <ul className="pl-[0] m-0 l-[-11px]">
+                    <li className="bg-[url('./assets/img/General.jpg')] block mb-[61px] pl-[98px] bg-no-repeat"></li>
+                    <li></li>
+                    <li></li>
+                </ul>
+            </div>
+        </>
+    )
+    
+}
+export default HowItWorks;
