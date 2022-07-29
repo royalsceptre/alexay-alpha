66f2dcc8f5bfed2e9da4a21e5ca5a41bb0776336
narakarax <liliyaogerenko@gmail.com>
2022-07-29 11:44:58 -0700

    APP-[7291144]

--- src/components/worksmanagement.tsx
+++ src/components/worksmanagement.tsx
@@ -3,9 +3,9 @@ const HowItWorks = () => {
         <>
             <div className="mt-[87px] mb-[93px]">
                 <div className="2xl:w-[calc(100%-811px)] xl:w-[calc(100%-700px)] pl-[125px] xl:pr-[48px]">
-                    <h2 className="text-[#333] text-[44px] font-extrabold m-[0 0 78px]">How it works</h2>
+                    <h2 className="text-[#333] text-[44px] font-extrabold mb-[78px]">How it works</h2>
                 <ul className="pl-[0] m-0 l-[-11px]">
-                    <li className="bg-[url('https://ww2-secure.justanswer.com/static/sip/btc/how-it-works/connect.svg')] block mb-[61px] pl-[98px] bg-no-repeat">
+                    <li className="bg-[url('https://ww2-secure.justanswer.com/static/sip/btc/how-it-works/connect.svg')] block mb-[61px] pl-[98px] bg-no-repeat bg-top-[17px] bg-left-[24px]">
                         <h3 className="text-[#000] text-[32px] leading-44px m-0">Connect</h3>
                         <p className="text-[#000] text-[20px] leading-[28px] m-0">
                         Tell the Experts' assistant what's going on so that she can find the best Expert for your situation.
