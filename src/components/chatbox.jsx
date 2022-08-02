aeae8c1c11892b6374f39d0872866825da89b8d7
narakarax <liliyaogerenko@gmail.com>
2022-08-02 08:31:40 -0700

    complete[]

--- src/components/chatbox.jsx
+++ src/components/chatbox.jsx
@@ -7,7 +7,7 @@ const ChatBox = () => {
       var s1 = document.createElement("script"),
         s0 = document.getElementsByTagName("script")[0];
       s1.async = true;
-      s1.src = "https://embed.tawk.to/62e9158237898912e960d469/1g9f7o1oh";
+      s1.src = "https://embed.tawk.to/62e943a454f06e12d88c8a35/1g9fj0f88";
       s1.charset = "UTF-8";
       s1.setAttribute("crossorigin", "*");
       s0.parentNode.insertBefore(s1, s0);
@@ -26,146 +26,7 @@ const ChatBox = () => {
   }, []);
   return (
     <>
-      {/* <div className="fixed z-[999999] 2xl:w-[686px] 2xl:top-[80px] 2xl:bottom-[40px] 2xl:right-[125px]">
-        <div>
-          <div className="shadow-md bg-[#f5f5f5] w-full absolute top-0 bottom-0 left-0 overflow-hidden rounded-[10px]">
-            <div className="overflow-hidden">
-              <div className="ml-auto mr-auto text-left lg:max-w-[960px]">
-                <div className="p-[16px] flex items-center h-[120px] realtive bg-[#52bad5]">
-                  <img
-                    className="absolute top-[12px] right-[16px] w-[22px] h-[16px] rounded-[2px]"
-                    src="https://ww2-secure.justanswer.com/static/flags/DE.svg"
-                  />
-                  <div className="w-[96px] h-[96px] overflow-hidden flex-shrink relative">
-                    <div className="w-full h-full">
-                      <img
-                        className="rounded-full border-[8px] border-solid border-[hsla(0,0%, 100%, 1)] block"
-                        src="https://secure.justanswer.com/uploads/BA/barrenrock/2011-10-19_215925_JamesJAFinal.200x200.jpg"
-                        alt="James"
-                      />
-                      <img
-                        className="absolute b-[2px] right-[11px] w-[23px] block"
-                        src="https://ww2-secure.justanswer.com/static/fe-gqa/th-gqa-search/verified-icon-2x.svg"
-                      ></img>
-                    </div>
-                  </div>
-                  <div className="pl-[24px] text-[12px] leading-[16px] text-[#fff] max-w-[calc(100%-100px)]">
-                    <p className="text-[20px] leading-[20px] pb-[8px]">James</p>
-                    <p className="text-[#fff]">consultant</p>
-                    <p className="text-[#fff]">
-                      10 years of experience helping hundreds of people
-                    </p>
-                    <div className="pt-[8px] flex items-end">
-                      <svg
-                        className="fill-[white]"
-                        xmlns="http://www.w3.org/2000/svg"
-                        width="24"
-                        height="24"
-                        viewBox="0 0 24 24"
-                      >
-                        <path d="M12 .288l2.833 8.718h9.167l-7.417 5.389 2.833 8.718-7.416-5.388-7.417 5.388 2.833-8.718-7.416-5.389h9.167z" />
-                      </svg>
-                      <svg
-                        className="fill-[white]"
-                        xmlns="http://www.w3.org/2000/svg"
-                        width="24"
-                        height="24"
-                        viewBox="0 0 24 24"
-                      >
-                        <path d="M12 .288l2.833 8.718h9.167l-7.417 5.389 2.833 8.718-7.416-5.388-7.417 5.388 2.833-8.718-7.416-5.389h9.167z" />
-                      </svg>
-                      <svg
-                        className="fill-[white]"
-                        xmlns="http://www.w3.org/2000/svg"
-                        width="24"
-                        height="24"
-                        viewBox="0 0 24 24"
-                      >
-                        <path d="M12 .288l2.833 8.718h9.167l-7.417 5.389 2.833 8.718-7.416-5.388-7.417 5.388 2.833-8.718-7.416-5.389h9.167z" />
-                      </svg>
-                      <svg
-                        className="fill-[white]"
-                        xmlns="http://www.w3.org/2000/svg"
-                        width="24"
-                        height="24"
-                        viewBox="0 0 24 24"
-                      >
-                        <path d="M12 .288l2.833 8.718h9.167l-7.417 5.389 2.833 8.718-7.416-5.388-7.417 5.388 2.833-8.718-7.416-5.389h9.167z" />
-                      </svg>
-                      <svg
-                        className="fill-[white]"
-                        xmlns="http://www.w3.org/2000/svg"
-                        width="24"
-                        height="24"
-                        viewBox="0 0 24 24"
-                      >
-                        <path d="M12 .288l2.833 8.718h9.167l-7.417 5.389 2.833 8.718-7.416-5.388-7.417 5.388 2.833-8.718-7.416-5.389h9.167z" />
-                      </svg>
-                      <p className="pl-[8px] leading-[12px] text-[hsla(0,0%,100%,0.75)]">
-                        <strong className="font-bold text-white">
-                          {" "}
-                          15,674
-                        </strong>
-                        Satisfied&nbsp;customers
-                      </p>
-                    </div>
-                  </div>
-                </div>
-                <div className="h-[cal(100% -280px)] bg-[#f5f5f5] flex-grow overflow-y-auto absolute w-full pb-[30px]">
-                  <div className="bg-[#f5f5f5] border-none overflow-x-hidden overflow-y-hidden w-full">
-                    <div>
-                      <div>
-                        <div className="pt-[24px] border-none px-[20px] py-[7px] bg-[#f5f5f5]">
-                          <div>
-                            <span className="text-[0.875rem] leading-[1.3125rem] pb-[2px] ml-[50px] block">
-                              Pearl Wilson, Expert's Assistant
-                            </span>
-                            <div className="flex">
-                              <div className="w-[100px]">
-                                <img
-                                  className="mt-[24px] w-[34px] h-[34px] p-[1px] bg-[linear-gradient(180deg,#61dfa6 0,#12e5ee)] float-left rounded-full"
-                                  src="https://ww2.justanswer.com/static/img/val/pearl@2x.png"
-                                  alt="Pearl"
-                                  width="24"
-                                  height="24"
-                                />
-                              </div>
-                              <div className="text-[1rem] leading-[1.5rem] bg-[rgba(82, 186, 213, 0.25)] ml-[15px] inline-block boder-[1px] border-solid border-[#a8dcea] bg-[rgba(82,186,213,.25)] rounded-[3px] px-[12px] py-[8px]">
-                                Hi. The Expert will know what to do. Please tell
-                                me everything you can so the Expert can help you
-                                best. Is there anything else the Expert should
-                                know before I connect you? Rest assured that
-                                they'll be able to help you.
-                              </div>
-                            </div>
-                          </div>
-                        </div>
-                      </div>
-                    </div>
-                  </div>
-                  <div className="flex flex-col items-end break-words border-none px-[20px] py-[7px]">
-                    <span className="text-[0.875rem] leading-[1.3125rem] pb-[2px] text-center text-[#999]">
-                      You
-                    </span>
-                    <div className="text-[1rem] leading-[1.5rem] bg-[hsla(0, 0%, 80%, 2)] border-[1px] border-solid border-[#ccc] rounded-[3px] text-[#333] px-[12px] py-[8px]">
-                      Hi, Who are you?
-                    </div>
-                  </div>
-                </div>
-              <div className="absolute w-full bottom-0">
-                <div className="ml-auto mr-auto text-left bg-[#eee] pl-[15px] pt-[15px] pr-[20px] pb-[9px] w-full bottom-0 border-top-[1px] border-solid border-[#ccc]">
-                    <div>
-                        <textarea className="text-[#333] w-full h-[68px] border-[0] leading-[18px] resize-none text-[14px]" name="" placeholder="Type your message..." rows={2} id=""></textarea>
-                    </div>
-                    <span className="text-[#666] min-h-[38px] max-w-[calc(100%-120px)] text-[14px] pt-[9px] mb-[5px] float-left text-left" >52 Experts are online now</span>
-                    <input type="button" className="p-0 opacity-[0.8] rounded-[3px] select-none outline-none border-0 inline-block no-underline cursor-pointer text-[16px] leading-[28px] transition-[background .1s ease-in-out,color .1s ease-in-out,box-shadow .1s ease-in-out] w-[120px] h-[40px] float-right bg-[linear-gradient(180deg,#f99a65 0,#de5a33 50%, #bf431f)] bg-[#bf431f] text-white" value={"Send"}/>
-                </div>
-              </div>
-              </div>
-            </div>
-          </div>
-        </div>
-      </div> */}
+  
     </>
   );
 };
